aac1851561eb61eb661fffad63591ce06da925a6
Alexxomby <72172506+Alexxomby@users.noreply.github.com>
2023-05-19 10:48:04 -0600

    qq

---
+++ 13Veterinaria/src/main/java/control_propietarios/CrearPropietario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package control_propietarios;
+
+import data_access_bd.PropietarioDAO;
+
+/**
+ *
+ * @author user
+ */
+public class CrearPropietario {
+    
+    try{
+ 
+        propietario = PropietarioDAO.save(propietario);
+
+}
+    
+}
